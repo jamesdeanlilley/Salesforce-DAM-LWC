ff5b338db296d7b37ca7c2a95ab661c5706c296c
James Dean Alexander Lilley <james@ravel.com>
2023-07-24 23:40:52 -0700

    Updated some boiler plate dynamic lwc code

--- force-app/main/default/lwc/sfDAMlwc/sfDAMlwc.js
+++ force-app/main/default/lwc/sfDAMlwc/sfDAMlwc.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getFinanceFields from '@salesforce/apex/AccountController.getFinanceFields';
+import getHealthcareFields from '@salesforce/apex/AccountController.getHealthcareFields';
+// Import more Apex methods for each industry
 
-export default class SfDAMlwc extends LightningElement {}
+export default class AccountFields extends LightningElement {
+    industryOptions = [
+        { label: 'Finance', value: 'Finance' },
+        { label: 'Healthcare', value: 'Healthcare' },
+        // Add more options for each industry
+    ];
+    selectedIndustries = [];
+
+    @wire(getFinanceFields)
+    financeFields;
+
+    @wire(getHealthcareFields)
+    healthcareFields;
+    // Add more @wire methods for each industry
+
+    handleIndustryChange(event) {
+        this.selectedIndustries = event.detail.value;
+    }
+}
